cb05a7af72c5ecc186c9860a6fbea03e743644cf
Sajad Hashemian <sajadhsm@gmail.com>
2022-02-12 02:33:19 +0330

    feat: new game

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
+import { useGame } from "../contexts/GameContext";
+
 const Header: React.VFC = () => {
+  const { dispatch } = useGame();
+
   return (
     <header className="flex justify-between items-center text-center border-b-2 border-gray-300 py-2 mb-4">
       <h1 className="text-xl font-bold">Mastermind</h1>
-      <button className="rounded-full py-1 px-2 text-xs font-bold hover:bg-gray-800 hover:text-white">
-        Restart
+      <button
+        className="rounded-full py-1 px-2 text-xs font-bold hover:bg-gray-800 hover:text-white"
+        onClick={() => dispatch({ type: "NEW_GAME" })}
+      >
+        New Game
       </button>
     </header>
   );
