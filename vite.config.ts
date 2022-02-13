c1d92f1bc4ee259d157ce7d77fb013bdeb2cdf4b
Sajad Hashemian <sajadhsm@gmail.com>
2022-02-13 12:53:04 +0330

    test: configure Vitest for unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+/// <reference types="vitest" />
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export const r = (...args: string[]) => resolve(__dirname, ...args)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+
+  root: r('src'),
+  resolve: {
+    alias: {
+      '@': r('src'),
+    },
+  },
+  
+  test: {
+    globals: true,
+    coverage: {
+      reportsDirectory: 'tests/coverage',
+    }
+  },
 })
